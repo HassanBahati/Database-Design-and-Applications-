efee9214f94ba37eea7717bff6f4c5de7333710e
HassanBahati <mukisabahati@gmail.com>
2022-06-16 11:22:19 +0300

    store the values under key Film in a set

--- movies.py
+++ movies.py
@@ -1,7 +1,17 @@
 import csv
 
+movies = set()
 with open("gross movies.csv", "r", encoding="UTF-8") as file:
     reader = csv.DictReader(file)
 
     for movie in reader:
+        #print all values with key Film
         print(movie["Film"])
+
+        #store the values under key Film in a set 
+        movies.add(movie["Film"])
+
+
+
+
+
