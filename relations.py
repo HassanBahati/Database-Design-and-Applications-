1a32eb8be5b06d042764675be1e0c349e9e539bf
HassanBahati <mukisabahati@gmail.com>
2022-06-23 11:51:45 +0300

    creat tables

--- relations.py
+++ relations.py
@@ -2,4 +2,17 @@ import csv
 from cs50 import SQL
 
 # open database 
-open("gross movies.db", "w").close()
+open("gross_movies_db.db", "w").close()
+
+db=SQL("sqlite:///gross_movies_db.db")
+
+# create table movies
+db.execute("CREATE TABLE movies (id INTEGER, title TEXT, PRIMARY KEY(id))")
+
+#create table genre
+db.execute("CREATE TABLE genre (movie_id INTEGER, genre TEXT, FOREIGN KEY(movie_id) REFERENCES movies(id))" )
+
+# inserting data into table
+with open("gross movies.csv", "r") as file:
+    #store in file called reader
+    reader = csv.DictReader(file)
