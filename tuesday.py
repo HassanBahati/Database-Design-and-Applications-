06aa4ca579beaeefd6e06d8579228cadec2b480e
HassanBahati <mukisabahati@gmail.com>
2022-06-28 17:06:30 +0300

    fix bug

--- tuesday.py
+++ tuesday.py
@@ -7,13 +7,15 @@ open('tuesday.db', 'w').close()
 
 db = SQL("sqlite:///tuesday.db")
 
-db.execute("CREATE TABLE movies (id INTEGER, title TEXT, PRIMARY KEY (id)")
+db.execute("CREATE TABLE movies (id INTEGER, title TEXT, PRIMARY KEY (id))")
 
 db.execute(
-    "CREATE TABLE genre (movie_id INTEGER, genre TEXT, FOREIGN KEY (movie_id) REFERENCES movie(id)")
+    "CREATE TABLE genre (movie_id INTEGER, genre TEXT, FOREIGN KEY (movie_id) REFERENCES movie(id))")
 
 with open("gross movies.csv", "r") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
         title = row["Film"].strip().capitalize()
+
+        db.execute("INSERT INTO movies (title) VALUES(?)", title)
