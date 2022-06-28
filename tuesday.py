bf5f9d43d6acce3ddefb17b44e7f323c975f3735
HassanBahati <mukisabahati@gmail.com>
2022-06-28 16:58:26 +0300

    create db

--- tuesday.py
+++ tuesday.py
@@ -7,3 +7,6 @@ open('tuesday.db', 'w').close()
 
 db = SQL("sqlite:///tuesday.db")
 
+db.execute("CREATE TABLE movies (id INTEGER, title TEXT, PRIMARY KEY (id)")
+
+db.execute("CREATE TABLE genre (movie_id INTEGER, genre TEXT, FOREIGN KEY (movie_id) REFERENCES movie(id)")
